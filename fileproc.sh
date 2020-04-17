037ff9677099a5b3eb452db5245ee42a0ecd9233
E.J. Bruce <ejbruce@us.ibm.com@ejs-macbook-pro.dfw.ibm.com>
2020-04-16 21:33:24 -0500

    Newlines

--- fileproc.sh
+++ fileproc.sh
@@ -2,6 +2,6 @@
 while read f1 f2
 do
 	sleep $f2
-	curl -s -k -o /dev/null -w "$f1\t%{time_connect}\t%{time_starttransfer}" -F "imagefile=@$(shuf -n 1 $2)" -H "$CURLHDR" $CURLURL | awk -F' ' '{print $1"\t"$3-$2}' &
+	curl -s -k -o /dev/null -w "$f1\t%{time_connect}\t%{time_starttransfer}\n" -F "imagefile=@$(shuf -n 1 $2)" -H "$CURLHDR" $CURLURL | awk '{print $1"\t"$3-$2}' &
 done < $1
 wait
--- filetrip.sh
+++ filetrip.sh
@@ -2,6 +2,6 @@
 while read f1 f2
 do
 	sleep $f2
-	curl -s -k -o /dev/null -w "$f1\t%{time_total}" -F "imagefile=@$(shuf -n 1 $2)" -H "$CURLHDR" $CURLURL &
+	curl -s -k -o /dev/null -w "$f1\t%{time_total}\n" -F "imagefile=@$(shuf -n 1 $2)" -H "$CURLHDR" $CURLURL &
 done < $1
 wait
